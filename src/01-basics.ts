755ba38b84cf5f23d61a9bc1641d5ba0218da30e
Sai Pradeepti <saipradeepti14@gmail.com>
2024-05-29 21:19:23 +0530

    Type Annotations

---
+++ src/01-basics.ts
@@ -0,0 +1,13 @@
+console.log("Hello! this is typescript basics")
+
+interface someValue{
+    name: string;
+    id: number;
+}
+
+let someObj: someValue = {
+    name: 'random',
+    id: 123,
+}
+
+console.log(someObj)
---
+++ src/02-type-annotations.ts
@@ -0,0 +1,24 @@
+// let awesome = 'shakeAndBake'
+// let amount = 20
+// let isAwesome = true
+
+let awesome:string = 'shakeAndBake'
+let amount:number = 20
+let isAwesome:boolean = true
+
+console.log(awesome, amount, isAwesome)
+
+// Challenge
+
+const testVar:string = "Hey there"
+console.log(testVar.toUpperCase())
+
+const testNum:number = 300.15
+console.log(Math.ceil(testNum))
+
+const testBool:boolean = true
+console.log(!testBool);
+
+
+
+
